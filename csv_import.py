b8a5d68b0d9ecdf512d045f0642b0e4a21894f97
resteve <resteve@zikzakmedia.com>
2013-12-02 15:01:26 +0100

    pyflakes

--- csv_import.py
+++ csv_import.py
@@ -1,7 +1,6 @@
 # This file is part of csv_sale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from decimal import Decimal
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
